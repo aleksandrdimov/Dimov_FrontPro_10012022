e37d49da65cf52eba8b7dc7c9c46aa3ec875e612
oleksandrdimov <vinstefol@gmail.com>
2023-02-08 15:57:10 +0200

    add task 20 and 21

---
+++ task_20_21.js
@@ -0,0 +1,28 @@
+// task 20
+function pow(num, degree) {
+  if (degree === 1) {
+    return num;
+  } else {
+    return num * pow(num, degree - 1);
+  }
+}
+
+console.log(pow(3, 3));
+
+// task 21
+let ladder = {
+  step: 0,
+  up: function () {
+    this.step++;
+    return this;
+  },
+  down: function () {
+    this.step--;
+    return this;
+  },
+  showStep: function () {
+    alert(this.step);
+  },
+};
+
+console.log(ladder.up().up().down().showStep());
