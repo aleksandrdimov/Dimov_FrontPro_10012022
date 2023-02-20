606437503566a0528efd30ed0211cd80298711f0
oleksandrdimov <vinstefol@gmail.com>
2023-02-20 17:51:23 +0200

    add task 27

--- task_27.js
+++ task_27.js
@@ -20,3 +20,88 @@ present та absent – ​​методи.
 Не забудьте після того, як напишите цей конструктор, створити 2-3 екземпляри (конкретних студентів)
 і показати використання цих методів.
 */
+
+class Student {
+  attendance = new Array(25);
+
+  constructor(firstName, lastName, yearOfBirth, gradesArray) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.yearOfBirth = yearOfBirth;
+    this.grades = gradesArray;
+  }
+
+  present() {
+    for (let i = 0; i < this.attendance.length; i++) {
+      if (this.attendance[i] === undefined) {
+        this.attendance[i] = true;
+        break;
+      }
+    }
+  }
+
+  absent() {
+    for (let i = 0; i < this.attendance.length; i++) {
+      if (this.attendance[i] === undefined) {
+        this.attendance[i] = false;
+        break;
+      }
+    }
+  }
+
+  viewStudentAge() {
+    console.log(`${this.firstName} : ${2023 - this.yearOfBirth} years`);
+  }
+
+  averageGrade() {
+    const calcGrade = (
+      this.grades.reduce((a, b) => a + b) / this.grades.length
+    ).toFixed(2);
+    console.log(`Средняя оценка ${calcGrade}`);
+    return calcGrade;
+  }
+
+  averageAttendance() {
+    const calcAttendance = (
+      this.attendance.filter((item) => item === true).length /
+      this.attendance.filter((item) => item === true || item === false).length
+    ).toFixed(2);
+    console.log(`Коэфициент посещаемости ${calcAttendance}`);
+    return calcAttendance;
+  }
+
+  summary() {
+    const numberGrade = this.averageGrade();
+    const numberAttendance = this.averageAttendance();
+    if (numberGrade > 90 && numberAttendance > 0.9) {
+      console.log("Молодець!");
+    } else if (numberGrade > 90 || numberAttendance > 0.9) {
+      console.log("Добре, але можна краще");
+    } else {
+      console.log("Редиска!");
+    }
+  }
+}
+
+const bob = new Student("Bob", "Smith", "2005", [95, 90, 99, 90]);
+const alice = new Student("Alice", "Ivanenko", "2006", [76, 91, 99, 89]);
+
+bob.present();
+bob.present();
+bob.present();
+bob.present();
+bob.absent();
+bob.present();
+bob.present();
+bob.viewStudentAge();
+bob.summary();
+console.log(bob);
+
+alice.present();
+alice.present();
+alice.absent();
+alice.present();
+alice.present();
+alice.viewStudentAge();
+alice.summary();
+console.log(alice);
